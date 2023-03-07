1bd2431c355cff92b1a1e708a3604ce35a21f7f5
VESGOBOY <ramonlool@hotmail.com>
2023-03-07 20:28:16 -0300

    Max Ammo IN Clip

--- client.lua
+++ client.lua
@@ -2,28 +2,27 @@ RedEM = exports["redem_roleplay"]:RedEM()
 --====================EVENT ADD AMMO========================--
 RegisterNetEvent('vesgoboy_ammo:giveammo')
 AddEventHandler('vesgoboy_ammo:giveammo', function(ammotype, result)
-    local playerPed = PlayerPedId()
-    local hasw, playerw = GetCurrentPedWeapon(playerPed,1)
+    local ped = PlayerPedId()
+    local hasw, playerw = GetCurrentPedWeapon(ped,1)
     for i,v in pairs(Config.Municoes) do
         for idarma, armas in pairs(v) do
             if ammotype == i then
                 local gunname = armas.name
                 local maxammo = armas.maxammo
-                if HasPedGotWeapon(PlayerPedId(),GetHashKey(gunname)) then
-                    local magazine = GetAmmoInPedWeapon(playerPed, gunname)
-                    local quantidademunicao = magazine + result
-                    if magazine >= maxammo then
+                if HasPedGotWeapon(ped,GetHashKey(gunname)) then
+                    local magazine = GetAmmoInPedWeapon(ped, gunname)
+                    local restante = maxammo - magazine -- calcula o número de munições que ainda podem ser adicionadas
+                    if restante == 0 then
                         TriggerEvent('redem_roleplay:Tip',"Você já tem o Maximo De Munições Equipadas. MAX:"..maxammo.." Munições Nessa Arma", 4000)
                     else
-                        if quantidademunicao <= maxammo then
-                            AddAmmoPed(gunname,ammotype,result)
-                            break
-                        elseif quantidademunicao > maxammo then
+                        local quantidademunicao = math.min(restante, result) -- adiciona apenas o restante ou o valor fornecido, o que for menor
+                        if quantidademunicao > 0 then
+                            AddAmmoPed(gunname, ammotype, quantidademunicao)
+                            TriggerEvent('redem_roleplay:Tip', quantidademunicao .. ' Munições adicionadas na arma', 4000)
+                        else
                             TriggerEvent('redem_roleplay:Tip',"Você não pode equipar mais de x"..maxammo.." Munições nessa Arma", 4000)
                         end
                     end
-                else
-                    TriggerEvent('redem_roleplay:Tip',"Você Precisa equipar sua arma antes de colocar munição nela..", 2000)
                 end
             end
         end
@@ -41,26 +40,28 @@ end
 --====================EVENT ADD ARROW========================--
 RegisterNetEvent('vesgoboy_ammo:givearrows')
 AddEventHandler('vesgoboy_ammo:givearrows', function(ammotype,result)
+    local ped = PlayerPedId()
     for ammoty,weapon in pairs(Config.Arrows) do
         if ammotype == ammoty then
             for bi,vi in pairs(weapon) do
                 local bowname = vi.name
                 local maxammo = vi.maxammo
-                local magazine = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(bowname))
-                local quantidademunicao = magazine + result
-                if magazine >= maxammo then
-                    TriggerEvent('redem_roleplay:Tip',"Você já tem o Maximo De Flechas Equipadas. o maximo desse arco com essa flecha é x"..maxammo, 4000)
-                else
-                    if quantidademunicao <= maxammo then
-                        if HasPedGotWeapon(PlayerPedId(),GetHashKey(bowname)) then
-                            AddArrowAmmo(bowname,ammotype,result)
-                            break
+                if HasPedGotWeapon(ped,GetHashKey(bowname)) then
+                    local magazine = GetAmmoInPedWeapon(ped, bowname)
+                    local restante = maxammo - magazine -- calcula o número de munições que ainda podem ser adicionadas
+
+                    if restante == 0 then
+                        TriggerEvent('redem_roleplay:Tip',"Você já tem o Maximo De Munições Equipadas. MAX:"..maxammo.." Munições Nessa Arma", 4000)
+                    else
+                        local quantidademunicao = math.min(restante, result) -- adiciona apenas o restante ou o valor fornecido, o que for menor
+                        if quantidademunicao > 0 then
+                            AddArrowAmmo(bowname,ammotype,quantidademunicao)
+                            TriggerEvent('redem_roleplay:Tip', quantidademunicao .. ' Munições adicionadas na arma', 4000)
                         else
-                            TriggerEvent('redem_roleplay:Tip',"Você Precisa Equipar um Arco Antes...", 4000)
+                            TriggerEvent('redem_roleplay:Tip',"Você não pode equipar mais de x"..maxammo.." Munições Nesse Arco", 4000)
                         end
-                    elseif quantidademunicao > maxammo then
-                        TriggerEvent('redem_roleplay:Tip',"Você não pode equipar mais de x"..maxammo.." dessa flecha nesse Arco", 4000)
                     end
+
                 end
             end
         end
@@ -99,7 +100,7 @@ AddEventHandler('vesgoboy_ammo:equiparammo', function(ammotype)
         end
     end
 end)
---====================EQUIPAR MUNIÇÕES========================--
+--====================FIM EQUIPAR MUNIÇÕES========================--
 --====================EQUIPAR FLECHAS========================--
 RegisterNetEvent('vesgoboy_ammo:equiparrow')
 AddEventHandler('vesgoboy_ammo:equiparrow', function(ammotype)
@@ -124,4 +125,37 @@ AddEventHandler('vesgoboy_ammo:equiparrow', function(ammotype)
         end
     end
 end)
---====================EQUIPAR FLECHAS========================--
+--====================FIM EQUIPAR FLECHAS========================--
+
+--[[
+    local magazine = GetAmmoInPedWeapon(playerPed, gunname)
+    local quantidademunicao = magazine + result
+    if magazine >= maxammo then
+        TriggerEvent('redem_roleplay:Tip',"Você já tem o Maximo De Munições Equipadas. MAX:"..maxammo.." Munições Nessa Arma", 4000)
+    else
+        if quantidademunicao <= maxammo then
+            AddAmmoPed(gunname,ammotype,result)
+            break
+        elseif quantidademunicao > maxammo then
+            TriggerEvent('redem_roleplay:Tip',"Você não pode equipar mais de x"..maxammo.." Munições nessa Arma", 4000)
+        end
+    end
+
+    local magazine = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(bowname))
+    local quantidademunicao = magazine + result
+
+    if magazine >= maxammo then
+        TriggerEvent('redem_roleplay:Tip',"Você já tem o Maximo De Flechas Equipadas. o maximo desse arco com essa flecha é x"..maxammo, 4000)
+    else
+        if quantidademunicao <= maxammo then
+            if HasPedGotWeapon(PlayerPedId(),GetHashKey(bowname)) then
+                AddArrowAmmo(bowname,ammotype,result)
+                break
+            else
+                TriggerEvent('redem_roleplay:Tip',"Você Precisa Equipar um Arco Antes...", 4000)
+            end
+        elseif quantidademunicao > maxammo then
+            TriggerEvent('redem_roleplay:Tip',"Você não pode equipar mais de x"..maxammo.." dessa flecha nesse Arco", 4000)
+        end
+    end
+]]
